8ccf0c00be7339323254b7b3d57a2500649fbba0
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-11 02:49:10 -0300

    aula da primeira semana

---
+++ test.js
@@ -0,0 +1,11 @@
+// var notas = [10,6,8,4,8]
+// notas.push(8)
+// console.log(notas.length)
+
+var valores = [9,7.8,5.6,10]
+var novoValor = [];
+
+valores.forEach(function(valor){
+  var valorDezPorcento = valor * 0.1
+  
+})
