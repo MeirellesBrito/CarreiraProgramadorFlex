1a990d5aa34e823c48516664f4430319f1cb94af
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-16 17:47:11 -0300

    foi feita as atividade 01 e 02  16/02/2023

--- exercicio.js
+++ exercicio.js
@@ -1,5 +1,4 @@
-// . Criar uma classe venda com os valores dos produtos daquela venda. No final
-// exibir o valor total da venda.
+// . Criar uma classe venda com os valores dos produtos daquela venda. No final exibir o valor total da venda.
 class venda{
     produdor;
 
@@ -10,7 +9,7 @@ class venda{
     calcularVendas(){
         let soma = 0
         this.produdor.forEach(function(valor){
-            soma= soma + valor;
+            soma+= valor;
         });
         return soma;
     }
@@ -19,51 +18,42 @@ var Venda = new venda([80,70,5]);
 // console.log("Valor do Produtor "+ Venda.calcularVendas()) 
 
 
+// 2. Criar uma classe funcionário com os atributos salário e cargo. Criar 10 objetos da classe funcionário e exibir a soma do salário dos funcionários de cargo Programador
 
-
-// 2. Criar uma classe funcionário com os atributos salário e cargo. Criar 10 objetos
-// da classe funcionário e exibir a soma do salário dos funcionários de cargo Programador
 class Funcionario {
-    constructor(salario, cargo) {
-      this.salario = salario;
-      this.cargo = cargo;
-    }
-    calcularSalario() {
-      return this.salario;
-    }
+  constructor(salario, cargo) {
+    this.salario = salario;
+    this.cargo = cargo;
   }
-  
-  class Programador extends Funcionario {
-    constructor(salario, cargo, Salario) {
-      super(salario, cargo);
-      this.Salario = Salario;
-    }
-    calcularSalario() {
-      return this.salario + this.Salario;
-    }
-  }
-  
-  var programador = new Programador(
-    3000, "Programador dev2 ",
-    4000, "Programador dev3 ",
-    5000, "Programador dev4 ",    
-    6000, "Programador dev2 ",   
-    7000, "Programador dev2 ",
-    8000, "Programador dev2 ",
-    9000, "Programador dev2 ",
-    10000, "Programador dev2 ",
-    11000, "Programador dev2 ",
-    12000, "Programador dev2 "
-    );
-  console.log(programador.calcularSalario());
-  
+}
+let funcionarios = [
+  new Funcionario(3000, "Gerente de Projetos"),
+  new Funcionario(2500, "Analista de Sistemas"),
+  new Funcionario(2000, "Desenvolvedor"),
+  new Funcionario(3500, "Marketing Manager"),
+  new Funcionario(2800, "Analista de Sistemas"),
+  new Funcionario(2200, "Analista de Sistemas"),
+  new Funcionario(4000, "Gerente de Projetos"),
+  new Funcionario(4500, "Programador"),
+  new Funcionario(2700, "Desenvolvedor"),
+  new Funcionario(5000, "Gerente de Projetos")
+];
 
 
-// 3. Criar duas classes, uma para Icarro e outra para moto. As classes devem ter os
-// atributos velocidadeAtual e velocidadeMaxima. Elas devem ter um método que irá acelear,
-// outro que irá freiar. Caso o freiar seja chamado, a velocidadeAtual deve ser zero. Caso
-// o acelerar seja chamado, a velocidadeAtual deverá ser a velocidadeAtual + 10, desde que
-// não ultrapasse a velocidade máxima. Usar conceito de herança, se necessário.
+let totalSalarioProgramador = funcionarios.reduce((total, funcionario) => {
+  if (funcionario.cargo === "Programador") {
+    return total + funcionario.salario;
+  } else {
+    return total;
+  }
+});
+
+// console.log("A soma dos salários dos Programadores e : " + totalSalarioProgramador);
+
+
+// 3. Criar duas classes, uma para Icarro e outra para moto. As classes devem ter os atributos velocidadeAtual e velocidadeMaxima. Elas devem ter um método que irá acelear, outro que irá freiar. Caso o freiar seja chamado, a velocidadeAtual deve ser zero. Caso
+// o acelerar seja chamado, a velocidadeAtual deverá ser a velocidadeAtual + 10, desde que não ultrapasse a velocidade máxima. Usar conceito de herança, se necessário.
+
 // 4. Criar uma classe turma que deve ter um atributo com os alunos daquela turma. No
 // atributo alunos deve ser guardado vários objetos de uma outra classe chamada aluno.
 // Dentro da classe aluno tem que guardar o nome e a média final do aluno. Criar um objeto
