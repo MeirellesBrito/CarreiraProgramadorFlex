ccad92a22bc05287c32f1b2042e65b24be593c0a
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-16 20:21:57 -0300

    foi feita as atividade 01,02,03,04 e 05  16/02/2023

--- exercicio.js
+++ exercicio.js
@@ -26,7 +26,7 @@ class Funcionario {
     this.cargo = cargo;
   }
 }
-let funcionarios = [
+let funcionarios = ([
   new Funcionario(3000, "Gerente de Projetos"),
   new Funcionario(2500, "Analista de Sistemas"),
   new Funcionario(2000, "Desenvolvedor"),
@@ -37,7 +37,7 @@ let funcionarios = [
   new Funcionario(4500, "Programador"),
   new Funcionario(2700, "Desenvolvedor"),
   new Funcionario(5000, "Gerente de Projetos")
-];
+]);
 
 
 let totalSalarioProgramador = funcionarios.reduce((total, funcionario) => {
@@ -48,13 +48,44 @@ let totalSalarioProgramador = funcionarios.reduce((total, funcionario) => {
   }
 });
 
-// console.log("A soma dos salários dos Programadores e : " + totalSalarioProgramador);
+// console.log("A soma dos salários dos Programadores e : "+ totalSalarioProgramador);
 
 
 // 3. Criar duas classes, uma para Icarro e outra para moto. As classes devem ter os atributos velocidadeAtual e velocidadeMaxima. Elas devem ter um método que irá acelear, outro que irá freiar. Caso o freiar seja chamado, a velocidadeAtual deve ser zero. Caso
 // o acelerar seja chamado, a velocidadeAtual deverá ser a velocidadeAtual + 10, desde que não ultrapasse a velocidade máxima. Usar conceito de herança, se necessário.
 
-// 4. Criar uma classe turma que deve ter um atributo com os alunos daquela turma. No
-// atributo alunos deve ser guardado vários objetos de uma outra classe chamada aluno.
-// Dentro da classe aluno tem que guardar o nome e a média final do aluno. Criar um objeto
+class Veiculo {
+  velocidadeAtual = 0;
+  velocidadeMaxima = 15;
+
+  acelerar() {
+    if ((this.velocidadeAtual + 10) <= this.velocidadeMaxima){
+      this.velocidadeAtual = this.velocidadeAtual + 10;
+    }else{
+      this.velocidadeAtual = this.velocidadeMaxima;
+    }
+  }
+  frear(){
+
+  }
+}
+class Carro extends Veiculo{
+  
+}
+class Moto extends Veiculo{
+  
+}
+
+var audi = new Carro ();
+audi.acelerar();
+console.log(audi.velocidadeAtual);
+// 4. Criar uma classe turma que deve ter um atributo com os alunos daquela turma. No atributo alunos deve ser guardado vários objetos de uma outra classe chamada aluno. Dentro da classe aluno tem que guardar o nome e a média final do aluno. Criar um objeto
 // da classe turma, preencher os alunos e exibir o nome do aluno com média maior que 7.
+class Turma{
+  alunos = [];
+
+}
+class aluno{
+  nome ;
+  media;
+}
