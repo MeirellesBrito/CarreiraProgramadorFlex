22f9f3724ecab1cdc0373037782fc6134d246049
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-03-02 20:30:49 -0300

    Flex | Inicio do curso da FLEX Full Carreira Programado Full Stack | front-end - 2023

--- Front_End/script/main.js
+++ Front_End/script/main.js
@@ -1,3 +1,9 @@
 function pesquisarCep(){
-    alert("executei")
+    var cep = document.querySelector("#cep").value
+    fetch("https://viacep.com.br/ws/"+cep+"/json/")
+    .then(response => response.json())
+    .then(response =>{
+        alert(response.logradouro)
+    })
+    
 }
